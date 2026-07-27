244206e989021a92a2bc337210d51de77c071c63
HerinOd <hieudo851@gmail.com>
2026-07-27 16:55:52 +0700

    4.3

--- src/main/java/config/ProjectConfiguration.java
+++ src/main/java/config/ProjectConfiguration.java
@@ -4,9 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan (
-        basePackages = {"services", "repositories", "proxies"}
-)
+@ComponentScan (basePackages = {"services", "repositories", "proxies"})
 public class ProjectConfiguration {
 
 }
